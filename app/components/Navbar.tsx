808eb1c3a7a794ae365efe60d438b5e4cbb39f97
Wayne Logan <loganamollo@gmail.com>
2024-11-20 14:22:48 +0300

    adds navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image"
+import { FaMap } from "react-icons/fa"
+
+export default function Navbar() {
+  return (
+    <nav className="
+    border-b
+    px-10
+    flex justify-between items-center
+    ">
+      <div className="
+        flex gap-2 items-center
+        text-2xl font-bold
+      ">
+        <FaMap />
+        <p>WhatBytes</p>
+      </div>
+      <div className="
+        border p-1 my-4 rounded-md
+        flex gap-2 items-center
+      ">
+        <Image className="rounded-full" src={'/avatar.png'} alt="avatar" width={30} height={30} />
+        <p className="font-semibold">
+          Rahil Siddique
+        </p>
+      </div>
+    </nav>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import {Inter} from "next/font/google"
+import Navbar from "./components/Navbar"
 
 export const metadata: Metadata = {
   title: "WhatBytes",
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body
         className={`${inter.className}`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
