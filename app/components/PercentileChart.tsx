aea926e9c93c2f65da4d0471cdc690ce617503ab
Wayne Logan <loganamollo@gmail.com>
2024-11-20 16:10:03 +0300

    removes unused imports

--- app/components/PercentileChart.tsx
+++ app/components/PercentileChart.tsx
@@ -1,13 +1,11 @@
 "use client"
 
-import { TrendingUp } from "lucide-react"
 import { CartesianGrid, Line, LineChart, ReferenceLine, XAxis } from "recharts"
 
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
