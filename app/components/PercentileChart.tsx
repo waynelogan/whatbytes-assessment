03df0d0c2040ee4f641495755b3a929a3098a0f4
Wayne Logan <loganamollo@gmail.com>
2024-11-20 16:03:57 +0300

    adds page center content

---
+++ app/components/PercentileChart.tsx
@@ -0,0 +1,107 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { CartesianGrid, Line, LineChart, ReferenceLine, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+const chartData = [
+  { percentile: 10, numOfStudents: 186 },
+  { percentile: 20, numOfStudents: 305 },
+  { percentile: 30, numOfStudents: 237 },
+  { percentile: 40, numOfStudents: 73 },
+  { percentile: 50, numOfStudents: 29 },
+  { percentile: 60, numOfStudents: 214 },
+  { percentile: 70, numOfStudents: 214 },
+  { percentile: 80, numOfStudents: 14 },
+  { percentile: 90, numOfStudents: 84 },
+  { percentile: 100, numOfStudents: 21 },
+]
+
+const chartConfig = {
+  numOfStudents: {
+    label: "Number of Students: ",
+    color: "#312e81",
+  }
+} satisfies ChartConfig
+
+export function PercentileChart() {
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>Comparison Graph</CardTitle>
+        <CardDescription>
+          <div className="flex justify-between">
+            <p>
+              <span className="font-semibold">You scored 30% percentile </span>
+              which is lower than the average percentile
+              72% of all the engineers who took this assessment.
+            </p>
+            <p className="text-2xl">📈</p>
+          </div>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <LineChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="percentile"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Line
+              dataKey="numOfStudents"
+              type="natural"
+              stroke="var(--color-numOfStudents)"
+              strokeWidth={2}
+              dot={{
+                fill: "var(--color-numOfStudents)",
+              }}
+              activeDot={{
+                r: 6,
+              }}
+            />
+            <ReferenceLine
+              x={30}
+              stroke="#312e81"
+              label={{ value: 'Your Percentile', position: 'center'}}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
+        <div className="flex gap-2 font-medium leading-none">
+          Trending up by 5.2% this percentile <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="leading-none text-muted-foreground">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,77 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import { PercentileChart } from "./components/PercentileChart"
+
 export default function Home() {
   return (
     <div className="
+      col-span-8
+      px-10 py-6
     ">
-      Landing Page
+      <h1>Skill Test</h1>
+      <div className="flex flex-col gap-4">
+        <div className="border rounded-md flex gap-2 w-full p-2">
+          <Image src={'/html.png'} alt="html logo" width={100} height={40} />
+          <div>
+            <h2 className="font-bold text-lg leading-loose">Hyper Text Markup Language</h2>
+            <p className="text-slate-700">Questions: 08 | Duration: 15 mins | Submitted on 5 June 2021</p>
+          </div>
+          <Button>
+            Update
+          </Button>
+        </div>
+        <div className="
+          border p-2 rounded-md
+        ">
+          <h2 className="font-bold text-lg leading-loose">Quick Statistics</h2>
+          <div className="flex justify-between divide-x">
+            <div className="flex gap-2">
+              <div className="
+                w-12 h-12 flex items-center justify-center text-xl
+                bg-slate-200 p-1 rounded-full
+              ">
+                <p>🏆</p>
+              </div>
+              <div>
+                <p className="font-bold">1</p>
+                <small className="text-slate-700">YOUR RANK</small>
+              </div>
+            </div>
+            <div className="flex gap-2 pl-2">
+              <div className="
+                w-12 h-12 flex items-center justify-center text-xl
+                bg-slate-200 p-1 rounded-full
+              ">
+                <p>🗒️</p>
+              </div>
+              <div>
+                <p className="font-bold">10/15</p>
+                <small className="text-slate-700">PERCENTILE</small>
+              </div>
+            </div>
+            <div className="flex gap-2 pl-2">
+              <div className="
+                w-12 h-12 flex items-center justify-center text-xl
+                bg-slate-200 p-1 rounded-full
+              ">
+                <p>✅</p>
+              </div>
+              <div>
+                <p className="font-bold">10/15</p>
+                <small className="text-slate-700">CORRECT ANSWERS</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="
+          border p-2 rounded-md
+        ">
+          <div>
+            
+            <PercentileChart />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
